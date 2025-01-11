64f9c78d1e2ac868f564b74decdda4ff151f4349
陈昊 <chenhao@chenhaodeMacBook-Pro.local>
2025-01-11 15:18:36 +0800

    fix: 修正API路径配置

--- src/config/api.js
+++ src/config/api.js
@@ -1,5 +1,5 @@
 const API_BASE_URL = {
-  MAIN_API: process.env.REACT_APP_BACKEND_API_URL || "https://art-back.hkg1.zeabur.app",     // 主后端API
+  MAIN_API: process.env.REACT_APP_BACKEND_API_URL || "https://art-back.hkg1.zeabur.app/api",     // 主后端API
   CRAWLER_API: process.env.REACT_APP_DAMAI_API_URL || "https://pa-m.zeabur.app",    // 大麦爬虫API
   SHOWSTART_API: process.env.REACT_APP_SHOWSTART_API_URL || "https://art-ss.hkg1.zeabur.app"    // 秀动爬虫API
 };
